9f3775280d180478bea41e4aec7b1f810674a31f
Jhffhhjjjee <113075640+Jhffhhjjjee@users.noreply.github.com>
2022-10-10 20:56:55 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -61,16 +61,16 @@ global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy
-global.owner = ['94714188730'] //ur number 
-global.ownertag = '94714188730' //ur tag number
-global.botname = '📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶' //ur bot name
-global.linkz = "https://youtu.be/3J9-8U8St3U" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/3J9-8U8St3U" //ur website to be displayed
+global.owner = ['94704296532'] //ur number 
+global.ownertag = '94704296532' //ur tag number
+global.botname = 'ChaThuRa MD BoT 😚🖇️' //ur bot name
+global.linkz = "https://wa.me/+94704296532" //your theme url which will be contact me whatsapp
+global.websitex = "https://wa.me/+94704296532" //ur website to be displayed
 global.botscript = 'https://github.com/Nipuna-apps/Blue-Lione-Bot' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark packname
-global.author = "📶 𝘽𝙡𝙪𝙚 𝙇𝙞𝙤𝙣𝙚 📶" //ur sticker watermark author
+global.packname = "ChaThuRa 😚🖇️" //ur sticker watermark packname
+global.author = "ChaThuRa 😚🖇️" //ur sticker watermark author
 global.menumoji ="💖"
 global.menuimg = "https://i.ibb.co/0mDLtyr/f2337149532299-58b7611d91faf.jpg" //ur alive img link [https://i.ibb.co]
 global.alive = "https://raw.githubusercontent.com/Nipuna-apps/Blue-Lion-Bot-V2/main/LioneMedia/video/Lione-bot.mp4" //ur alive video or image link [github.com]
